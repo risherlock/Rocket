4daf8ae9308e0fcad7ea62e2bfef669a28df8413
Rishav Dhungel <62413199+risherlock@users.noreply.github.com>
2020-09-08 17:28:06 +0545

    Update rocketDynamicalModel.m

--- rocket/rocketDynamicalModel.m
+++ rocket/rocketDynamicalModel.m
@@ -2,17 +2,22 @@ function [state_dot] = rocketDynamicalModel(~,state,gimble_state)
 % Rishav (2020/9/8)
 
 % Unpack variables
-ypr = state(1:3); % Orientation [yaw,pitvh,roll]'
-omegas = state(4:6); % Angular velocities
+ypr = state(1:3); % Orientation vector
+omega = state(4:6); % Angular velocity vector
+position = state(7:9); % Position vector
+velocity = state(10:12); % Translational velocity vector
 
+% Compute force and moments (Body frame)
 F_p = computePropulsiveForce(thrust,gimble_state);
-F_g = computeGravitationalForce(mass,height,ypr);
+F_g = computeGravitationalForce(mass,position,ypr);
 F_a = 0;
 tau = computeControlMoment(thrust,gimble_state);
 
-ypr_dot = eulerKinematics(t,ypr,omegas);
-pqr_dot = rocketKinetics(t,omegas,tau);
-uvw_dot = rocketTranslationKinetics(t,vels,omegas,F_a,F_g,F_p);
+% Propagate states
+ypr_dot = rocketRotationalKinematics(t,ypr,omega);
+omega_dot = rocketRotationalKinetics(t,omega,tau);
+position_dot = velocity;
+velocity_dot = rocketTranslationKinetics(t,velocity,omega,F_a,F_g,F_p); 
 
-state_dot = [ypr_dot',pqr_dot'];
+state_dot = [ypr_dot',omega_dot',position_dot',velocity_dot'];
 end
