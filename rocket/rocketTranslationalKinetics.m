ab2a93f60c7e69be45ff64ee29106b8408e09b9f
Rishav Dhungel <62413199+risherlock@users.noreply.github.com>
2020-09-08 17:28:58 +0545

    Create rocketTranslationalKinetics.m

---
+++ rocket/rocketTranslationalKinetics.m
@@ -0,0 +1,17 @@
+function [acceleration] = rocketTranslationalKinetics(~,velovity,omega,F_a,F_g,F_p)
+%%% Compute the translational acceleration of the rocket
+%
+% Source:
+%   [1] Ashish Tiwari - Advanced Control of Aircraft, Spacecraft and Rockets
+%   [2] Military Handbook - Missile Flight Simulation Part One: 
+%       Surface-to-Air Missiles (1995)
+%   [3] Kisabo - State-Space Modelling of a Rocket for Optimal Control 
+%       System Design (2019)
+%
+% Rishav (2020/9/8)
+
+%%% Translational Dynamics differential equation
+acc_inertial = (F_a + F_p + F_g)/m; % In inertial frame
+acc_body = acc_inertial - cross(omega,velovity); % Transport theorem
+acceleration = acc_body; 
+end
