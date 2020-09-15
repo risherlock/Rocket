a5c43671f2db621d662357072d1d6a06dfb59e3b
Rishav Dhungel <62413199+risherlock@users.noreply.github.com>
2020-09-15 22:49:21 +0545

    Update computeAerodynamicForce.m

--- ext_forces/computeAerodynamicForce.m
+++ ext_forces/computeAerodynamicForce.m
@@ -1,9 +1,16 @@
-function [F_a] = computeAerodynamicForce(velocity,area)
+function [F_a] = computeAerodynamicForce(velocity,area,rho)
 %%% Computes aerodynamic force in body frame
 % Rishav (2020/9/10)
 
-% Normal force coefficients
-v = norm(velocity);
+aero = configRocket; % Access rocket config
+Ca = aero.Ca; % Axial force coefficient
+Cn = aero.Cn; % Normal force coefficient
+
+V = norm(velocity);
+% u = velocity(1);
+v = velocity(2);
+w = velocity(3);
+
 Cny = -Cn*v/sqrt(v^2 + w^2);
 Cnz = -Cn/sqrt(v^2 + w^2);
 
@@ -11,5 +18,5 @@ F_x = -0.5*rho*V^2*Ca*area;
 F_y = 0.5*rho*V^2*Cny*area;
 F_z = 0.5*rho*V^2*Cnz*area;
 
-F_a = [F_x,F_y,F_z];
+F_a = [F_x,F_y,F_z]';
 end
