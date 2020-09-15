d1ab8be46ad8330be09234a019e1fa910908b5c0
Rishav Dhungel <62413199+risherlock@users.noreply.github.com>
2020-09-16 02:09:46 +0545

    Update configSimulation.m

--- configs/configSimulation.m
+++ configs/configSimulation.m
@@ -8,10 +8,11 @@ sim.stop_time = 15; % sec
 sim.dt = 0.01; % sec
 
 % Initial conditions in inertial frame
-init_ypy = [0.1, 0.2, 0.3];
-init_omega = [0.1, 0.2, 0.3];
-init_position = [0.1, 0.2, 0.3];
-init_velocity = [0.1, 0.2, 0.3];
-sim.init_state = [init_ypy,init_omega, ...
-    init_position,init_velocity]';
+init_ypy = [0.1, 0.2, 0.3]; % radians
+init_omega = [0.1, 0.2, 0.3]; % rad/sec
+init_position = [0.1, 0.2, 0.3]; % m
+init_velocity = [0.1, 0.2, 0.3]; % m/s
+sim.init_state = [init_ypy, init_omega, ...
+    init_position,init_velocity]'; % State vector, 12x1
+sim.init_gimble_state = [0,0]; % radians
 end
