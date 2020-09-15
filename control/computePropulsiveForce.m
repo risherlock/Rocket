527c2afe471a6778e42815702136fd0fcc2febc0
Rishav Dhungel <62413199+risherlock@users.noreply.github.com>
2020-09-15 22:48:33 +0545

    Minor edits

--- control/computePropulsiveForce.m
+++ control/computePropulsiveForce.m
@@ -1,7 +1,7 @@
 function [F_p] = computePropulsiveForce(Fp,gimble_state)
 % Rishav (2020/9/8)
 
-% Unpack gimble angles
+% Unpack gimble angles (radians)
 mu_1 = gimble_state(1);
 mu_2 = gimble_state(2);
 
@@ -11,5 +11,5 @@ F_py = Fp*cos(mu_2)*sin(mu_2);
 F_pz = -Fp*sin(mu_2);
 
 % Propulsive force vector
-F_p = [F_px,F_py,F_pz];
+F_p = [F_px,F_py,F_pz]';
 end
