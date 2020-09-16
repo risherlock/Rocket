4211d96e3b234e81247ee094e82cb32e32ee329e
Rishav Dhungel <62413199+risherlock@users.noreply.github.com>
2020-09-16 17:02:22 +0545

    Update rocketTranslationalKinetics.m

--- rocket/rocketTranslationalKinetics.m
+++ rocket/rocketTranslationalKinetics.m
@@ -1,7 +1,7 @@
 function [acc_inertial] = ...
     rocketTranslationalKinetics(~,velocity,omega,mass,F_a,F_g,F_p)
 %%% Compute the translational acceleration of the rocket in inertial frame
-% Rishav (2020/9/8)
+% 2020/9/8
 
 acc_body = (F_a + F_p + F_g)/mass;
 acc_inertial = acc_body - cross(omega,velocity); % Transport theorem
